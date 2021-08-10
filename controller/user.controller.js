d2fab887fdd9eb851cb23f07fa8343a4ae66a501
nguyendv99 <43127898+nguyendv99@users.noreply.github.com>
2021-08-10 20:41:02 +0700

    Update user.controller.js

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -3,93 +3,85 @@ const jwt = require('jsonwebtoken')
 const config = require('./../config')
 var listToken = {}
 
-exports.login = async (req, res) => {
-
+exports.login = (req, res) => {
     var username = req.body.username;
     var password = req.body.password;
 
-    db.beginTransaction(function (err) {
-        if (err) { throw err; }
-        db.query('SELECT * FROM user WHERE username = ? AND password = ?', [username, password], function (error, results, fields) {
-            if (error) {
-                return connection.rollback(function () {
-                    throw error;
-                });
-            }
-            if (results.length > 0) {
+    // Kiểm tra tài khoản có tồn tại hay không
+    sql = "SELECT * FROM user WHERE username = ? AND password = ?"
+    db.query(sql, [username, password], (err, user) => {
+        if (err) throw err;
+        if (user.length > 0) {
+            // Tạo token cho user
+            let token = jwt.sign({
+                username: user[0].username,
+            }, config.secret, {
+                expiresIn: config.tokenLife,
+            })
 
-                let token = jwt.sign({
-                    username: results[0].username,
-                }, config.secret, {
-                    expiresIn: config.tokenLife,
-                })
+            // Thêm token vào danh sách
 
-                db.query('UPDATE user SET token = ? WHERE user_id = ?', [token, results[0].user_id], function (error, results, fields) {
-                    if (error) {
-                        return connection.rollback(function () {
-                            throw error;
-                        });
-                    }
+            listToken[user[0].username] = token
 
-                });
-                return res.json({
-                    username: results[0].username,
-                    token: token
-                })
-            } else {
-                return res.json("Đăng nhập không thành công")
-            }
+            // Trả về thông tin người dùng và token
 
-        });
+            return res.json({
+                username: user[0].username,
+                token: token
+            })
+        } else {
+            return res.json("Đăng nhập không thành công")
+        }
 
-    });
+    })
 
 }
 
 exports.checkLogin = (req, res, next) => {
+
     try {
+        // Kiểm tra token mà người dùng gửi lên có tồn tại hay không
         var token = req.headers.token;
         const data = jwt.verify(token, config.secret)
         if (data) {
             var username = data.username
-            sql = 'SELECT * FROM user WHERE username = ?';
-            console.log("token: ")
-            db.query(sql, username, (err, user) => {
-                console.log("token: " + user)
-                if (user[0].token === token) {
-                    next()
-                } else {
-                    res.send("bạn cần đăng nhập")
-                }
-            })
+            // Kiểm tra tính duy nhất của token, người dùng chỉ có thể đăng nhập trên 1 thiết bị
+            if (listToken[username] === token) {
+                next()
+            } else {
+                res.send("bạn cần đăng nhập")
+            }
         }
     } catch (error) {
         res.send("bạn cần đăng nhập")
     }
+
 }
 
 
 exports.checkAdmin = (req, res, next) => {
     try {
+        // Kiểm tra token mà người dùng gửi lên có tồn tại hay không
         var token = req.headers.token;
         const data = jwt.verify(token, config.secret)
         if (data) {
             var username = data.username
-            sql = 'SELECT * FROM user WHERE username = ? AND level = ?';
-            console.log("token: ")
-            db.query(sql, [username, 1], (err, user) => {
-                console.log("token: " + user)
-                if (user[0].token === token) {
-                    next()
-                } else {
-                    res.send("bạn cần đăng nhập")
-                }
-            })
+            // Kiểm tra tính duy nhất của token, người dùng chỉ có thể đăng nhập trên 1 thiết bị
+            if (listToken[username] === token) {
+                // Kiểm tra level người dùng
+                sql = 'SELECT * FROM user WHERE username = ? AND level = ?';
+                db.query(sql, [username, 1], (err, user) => {
+                    if (user.length > 0) {
+                        next()
+                    } else {
+                        res.send("Bạn không có quyền")
+                    }
+                })
+            } else {
+                res.send("bạn cần đăng nhập")
+            }
         }
     } catch (error) {
         res.send("bạn cần đăng nhập")
     }
 }
-
-
-
